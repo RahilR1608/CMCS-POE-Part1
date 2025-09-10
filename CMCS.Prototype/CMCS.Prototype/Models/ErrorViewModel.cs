8ebc2ab83844041c1affe11c191fd72ae3380bfb
RahilR1608 <st10434494@vcconnect.edu.za>
2025-09-10 21:02:57 +0200

    chore: add .gitignore to ignore VS cache and build folders

---
+++ CMCS.Prototype/CMCS.Prototype/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CMCS.Prototype.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
